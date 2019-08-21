3c081f255c6eb5812f1b16c5c88fd4a7b8ed8c02
igarcia526 <garciai0526@hotmail.com>
2019-08-21 18:11:17 -0400

    first push

---
+++ models/weather.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+    const Weather = sequelize.define('weather', {
+        city: {
+            type: DataTypes.STRING, 
+            allowNull: false, 
+        },
+        current_location: {
+            type: DataTypes.BOOLEAN, 
+            allowNull: false, 
+        },
+        user_ID: {
+            type: DataTypes.INTEGER,
+        },
+    })
+
+    return Weather;
+}
