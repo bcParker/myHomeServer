f99107d1f0671196dfc5a46973ac056ad286d8ab
bcParker <cparker8890@gmail.com>
2019-08-21 18:30:39 -0400

    created user model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+module.exports = function(sequelize, DataTypes) {
+	const User = sequelize.define('user', {
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			uniqe: true
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: true
+		},
+		avatar: {
+			type: DataTypes.STRING,
+			allowNull: true
+		}
+	})
+}
+
+return User;
