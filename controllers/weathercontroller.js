7dbc96deef4599850f1338407bd811835412ce36
bcParker <cparker8890@gmail.com>
2019-08-28 20:56:53 -0400

    updated weatherController endpoints

--- controllers/weathercontroller.js
+++ controllers/weathercontroller.js
@@ -4,16 +4,16 @@ const Weather = sequelize.import('../models/weather');
 
 router.post('/add', (req, res) => {
 	Weather.create({
-		city: req.body.weather.city, //Format of request body might change
+		city: req.body.city, //Format of request body might change
 		current_location: req.body.current_location, //Fromat of request body might change
-		user: req.user.id
+		user: req.user
 	})
 	.then(log => res.status(200).json({Message: 'Saved City'}))
 	.catch(err => res.status(500))
 })
 
-router.put('/changeLocation', (req, res) => {
-	Weather.update(req.body, {where: {current_location: req.body.current_location}})
+router.put('/changeLocation/:id', (req, res) => {
+	Weather.update(req.body, {where: {id: req.params.id}})
 		.then(log => res.status(200).json({ Message: 'Current Location Set (و ˃̵ᴗ˂̵)و'}))
 		.catch(err => res.status(500).json({Error: 'failed to update'}))
 })
@@ -25,7 +25,7 @@ router.get('/display', (req, res) => {
 })
 
 router.delete('/delete', (req, res) => {
-	Weather.destroy({where: {city: req.body.weather.city}})
+	Weather.destroy({where: {city: req.body.city}})
 		.then(log => res.status(200).json({ Message: 'Successfully deleted (و ˃̵ᴗ˂̵)و'}))
 		.catch(log => res.status(500).json({Error: 'failed to delete'}))
 })
